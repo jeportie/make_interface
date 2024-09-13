e2c56194ab95aec672ada641296f12282ab11eeb
Jerome Portier <jeportie@made-f0Br8s1.clusters.42paris.fr>
2024-09-13 10:10:36 +0200

    adding progresion bar

--- progress.c
+++ progress.c
@@ -6,7 +6,7 @@
 /*   By: jeportie <jeportie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 19:31:14 by jeportie          #+#    #+#             */
-/*   Updated: 2024/09/12 19:37:29 by jeportie         ###   ########.fr       */
+/*   Updated: 2024/09/13 10:08:59 by jeportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void	print_progress(int step, int total)
 		step = total;
 	percent = (step * 100) / total;
 	bar_length = (step * BAR_WIDTH) / total;
-	printf("\r\033[32m[%-*.*s] %d%%\033[0m", BAR_WIDTH, bar_length,
+	printf("\r\033[32m[%-*.*s] %d%%\n\033[0m", BAR_WIDTH, bar_length,
 		"####################", percent);
 	fflush(stdout);
 }
