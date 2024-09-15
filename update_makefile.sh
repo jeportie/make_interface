a268c38bcc4138599676cd8172a51a6cd644735d
jeromep <jeromevdb.production@gmail.com>
2024-09-15 19:30:20 +0200

    makefile update

--- update_makefile.sh
+++ update_makefile.sh
@@ -1,7 +1,20 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    update_makefile.sh                                 :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jeportie <jeportie@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/09/15 19:28:59 by jeportie          #+#    #+#              #
+#    Updated: 2024/09/15 19:29:01 by jeportie         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 #!/bin/bash
 
 # Define the root directory
 SRC_DIR="src"
+BAK_DIR=".bak"
 
 # Function to generate the list of .c files recursively in src/ and subdirectories
 generate_src_list() {
@@ -18,8 +31,14 @@ END_MARKER="### END AUTO GENERATED FILES ###"
 # Prepare the format of the file list for the Makefile
 FILE_LIST=""
 for FILE in $SRC_FILES; do
-  FILE_LIST="$FILE_LIST \\\n  $FILE"
+  FILE_LIST="$FILE_LIST  $FILE \\\n"
 done
+
+# Remove the trailing '\' from the last file
+FILE_LIST=$(echo -e "$FILE_LIST" | sed '$ s/\\//')
+
+# Ensure the .bak folder exists
+mkdir -p $BAK_DIR
 
 # Check if the Makefile exists and contains the marker for auto-generated files
 if grep -q "$START_MARKER" Makefile; then
@@ -32,4 +51,7 @@ else
   echo -e "\n$START_MARKER\n# List of source files:\nSRC = $FILE_LIST\n$END_MARKER" >> Makefile
 fi
 
-echo "Makefile updated with new .c files."
+# Move the backup Makefile to the .bak folder
+mv Makefile.bak $BAK_DIR/
+
+echo "Makefile updated with new .c files. Backup stored in .bak/"
