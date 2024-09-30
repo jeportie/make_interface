310aeb1fbe25d448a7b5a04c5553c61462bd4d4e
Jerome Portier <jeportie@made-f0Br5s8.clusters.42paris.fr>
2024-09-30 17:05:16 +0200

    push pause

---
+++ exec/clean_test.sh
@@ -0,0 +1,35 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    clean_test.sh                                      :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jeportie <jeportie@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/09/30 17:04:56 by jeportie          #+#    #+#              #
+#    Updated: 2024/09/30 17:05:03 by jeportie         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#!/bin/bash
+
+# Find the test_src directory
+test_src_dir=$(find . -type d -name "test_src")
+
+# Check if the test_src directory exists
+if [ -d "$test_src_dir" ]; then
+    echo "Found test_src directory at: $test_src_dir"
+    
+    # Navigate to the test_src directory
+    cd "$test_src_dir"
+
+    # Remove the specified files and directories
+    echo "Removing specified files and directories..."
+    rm -rf CMakeCache.txt Makefile cmake_install.cmake CMakeFiles CTestTestfile.cmake Testing
+
+    # Remove all compiled executables (files with no extension)
+    find . -type f -executable -exec rm -f {} +
+
+    echo "Cleanup complete."
+else
+    echo "test_src directory not found."
+fi
